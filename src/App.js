f5a7206c8c4f20c885d94f766f1c540ebba0fe1b
Damjan Kovacic <letecnez@gmail.com>
2018-09-20 14:05:56 +0200

    bootstrap and react-bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Alert from "react-bootstrap/lib/Alert";
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<p>Damjan Kovačič</p>
+				<Alert bsSize="danger">Test</Alert>
 			</div>
 		);
 	}

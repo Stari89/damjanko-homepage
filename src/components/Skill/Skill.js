96c077aab8db529324524508b929565b11030bb8
Damjan Kovacic <letecnez@gmail.com>
2018-10-01 22:53:31 +0200

    Skills popout with value

--- src/components/Skill/Skill.js
+++ src/components/Skill/Skill.js
@@ -3,43 +3,44 @@ import { Col } from "reactstrap";
 import "./Skill.css";
 
 export class Skill extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.handleMouseEnter = this.handleMouseEnter.bind(this);
-    this.handleMouseLeave = this.handleMouseLeave.bind(this);
-    this.state = { mouseOver: false };
-  }
+		this.handleMouseEnter = this.handleMouseEnter.bind(this);
+		this.handleMouseLeave = this.handleMouseLeave.bind(this);
+		this.state = { mouseOver: false };
+	}
 
-  handleMouseEnter() {
-    console.log("mouseEnter");
-    this.setState({ mouseOver: true });
-  }
+	handleMouseEnter() {
+		this.setState({ mouseOver: true });
+	}
 
-  handleMouseLeave() {
-    console.log("mouseLeave");
-    this.setState({ mouseOver: false });
-  }
+	handleMouseLeave() {
+		this.setState({ mouseOver: false });
+	}
 
-  render() {
-    return (
-      <Col
-        xs="3"
-        sm="2"
-        lg="1"
-        className={this.state.mouseOver ? "py-3 popout" : "py-3"}
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
-      >
-        <div className="skill-image mb-2">
-          <img
-            src={this.props.logoSrc}
-            className="img-fluid"
-            alt={this.props.content}
-          />
-        </div>
-        <div className="text-center font-weight-bold">{this.props.content}</div>
-      </Col>
-    );
-  }
+	render() {
+		const skillClass = "skill py-3 rounded skill-" + this.props.skillValue;
+		return (
+			<Col
+				xs="3"
+				sm="2"
+				lg="1"
+				className={
+					this.state.mouseOver ? skillClass + " popout shadow" : skillClass
+				}
+				onMouseEnter={this.handleMouseEnter}
+				onMouseLeave={this.handleMouseLeave}
+			>
+				<div className="skill-image mb-2">
+					<img
+						src={this.props.logoSrc}
+						className="img-fluid"
+						alt={this.props.content}
+					/>
+				</div>
+				<div className="text-center font-weight-bold">{this.props.content}</div>
+			</Col>
+		);
+	}
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,27 +5,28 @@ import { Article } from "./Article";
 import data from "./../data";
 
 const renderContent = content => {
-  return (
-    <React.Fragment>
-      {content.map(section => {
-        if (section.type === "Row") {
-          return <Row key={section.key}>{renderContent(section.content)}</Row>;
-        }
-        if (section.type === "Skill") {
-          return (
-            <Skill
-              key={section.key}
-              logoSrc={section.logoSrc}
-              content={section.content}
-            />
-          );
-        }
-        return <Article key={section.key} section={section} />;
-      })}
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			{content.map(section => {
+				if (section.type === "Row") {
+					return <Row key={section.key}>{renderContent(section.content)}</Row>;
+				}
+				if (section.type === "Skill") {
+					return (
+						<Skill
+							key={section.key}
+							logoSrc={section.logoSrc}
+							content={section.content}
+							skillValue={section.skillValue}
+						/>
+					);
+				}
+				return <Article key={section.key} section={section} />;
+			})}
+		</React.Fragment>
+	);
 };
 
 export const Skills = () => {
-  return <Container>{renderContent(data.skills.content)}</Container>;
+	return <Container>{renderContent(data.skills.content)}</Container>;
 };

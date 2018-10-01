e7a8343c3d03dd1fdec3a4dd06583b7a35e3d841
Damjan Kovačič <letecnez@gmail.com>
2018-10-01 16:39:40 +0200

    Skill changes class name if mouseOver state

--- src/components/Skill/Skill.js
+++ src/components/Skill/Skill.js
@@ -1,13 +1,45 @@
 import React from "react";
 import { Col } from "reactstrap";
 import "./Skill.css";
-export const Skill = props => {
-	return (
-		<Col xs="3" sm="2" lg="1" className="py-3">
-			<div className="skill-image mb-2">
-				<img src={props.logoSrc} className="img-fluid" alt={props.content} />
-			</div>
-			<div className="text-center font-weight-bold">{props.content}</div>
-		</Col>
-	);
-};
+
+export class Skill extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+    this.state = { mouseOver: false };
+  }
+
+  handleMouseEnter() {
+    console.log("mouseEnter");
+    this.setState({ mouseOver: true });
+  }
+
+  handleMouseLeave() {
+    console.log("mouseLeave");
+    this.setState({ mouseOver: false });
+  }
+
+  render() {
+    return (
+      <Col
+        xs="3"
+        sm="2"
+        lg="1"
+        className={this.state.mouseOver ? "py-3 popout" : "py-3"}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+      >
+        <div className="skill-image mb-2">
+          <img
+            src={this.props.logoSrc}
+            className="img-fluid"
+            alt={this.props.content}
+          />
+        </div>
+        <div className="text-center font-weight-bold">{this.props.content}</div>
+      </Col>
+    );
+  }
+}

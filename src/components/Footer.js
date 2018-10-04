2a2ccdd2b4db92c747f110165daabe0a572b933d
Damjan Kovačič <letecnez@gmail.com>
2018-10-04 13:02:33 +0200

    GitLab links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,141 +4,156 @@ import { HashLink } from "react-router-hash-link";
 import ReactGA from "react-ga";
 
 export class Footer extends React.Component {
-	handleIntroductionClick() {
-		ReactGA.event({ category: "footer", action: "introduction-click" });
-	}
+  handleIntroductionClick() {
+    ReactGA.event({ category: "footer", action: "introduction-click" });
+  }
 
-	handleExperienceClick() {
-		ReactGA.event({ category: "footer", action: "experience-click" });
-	}
+  handleExperienceClick() {
+    ReactGA.event({ category: "footer", action: "experience-click" });
+  }
 
-	handleSkillsClick() {
-		ReactGA.event({ category: "footer", action: "skills-click" });
-	}
+  handleSkillsClick() {
+    ReactGA.event({ category: "footer", action: "skills-click" });
+  }
 
-	handleEmailClick() {
-		ReactGA.event({ category: "footer", action: "email-click" });
-	}
+  handleEmailClick() {
+    ReactGA.event({ category: "footer", action: "email-click" });
+  }
 
-	handleLinkedInClick() {
-		ReactGA.event({ category: "footer", action: "linkedin-click" });
-	}
+  handleLinkedInClick() {
+    ReactGA.event({ category: "footer", action: "linkedin-click" });
+  }
 
-	handleFacebookClick() {
-		ReactGA.event({ category: "footer", action: "facebook-click" });
-	}
+  handleFacebookClick() {
+    ReactGA.event({ category: "footer", action: "facebook-click" });
+  }
 
-	handleGitHubClick() {
-		ReactGA.event({ category: "footer", action: "github-click" });
-	}
+  handleGitHubClick() {
+    ReactGA.event({ category: "footer", action: "github-click" });
+  }
 
-	render() {
-		return (
-			<Container>
-				<Row className="py-4">
-					<Col xs="12" md="6">
-						<h3>
-							<small>About</small>
-						</h3>
-						<p>
-							This site was built with React.js. It is hosted by an Apache
-							server, running on my personal NAS device.
-						</p>
-						<p>
-							<a
-								href="https://github.com/Stari89/damjanko-homepage"
-								className="text-light"
-								onClick={this.handle}
-							>
-								<u>Pull this project from GitHub!</u>
-							</a>
-						</p>
-					</Col>
-					<Col xs="6" md="3">
-						<h3>
-							<small>Navigation</small>
-						</h3>
-						<ul className="list-unstyled">
-							<li>
-								<HashLink
-									to="/#introduction"
-									className="text-light"
-									onClick={this.handleIntroductionClick}
-								>
-									<u>Introduction</u>
-								</HashLink>
-							</li>
-							<li>
-								<HashLink
-									to="/#experience"
-									className="text-light"
-									onClick={this.handleExperienceClick}
-								>
-									<u>Experience</u>
-								</HashLink>
-							</li>
-							<li>
-								<HashLink
-									to="/#skills"
-									className="text-light"
-									onClick={this.handleSkillsClick}
-								>
-									<u>Skills</u>
-								</HashLink>
-							</li>
-						</ul>
-					</Col>
-					<Col xs="6" md="3">
-						<h3>
-							<small>Contact me</small>
-						</h3>
-						<ul className="list-unstyled">
-							<li>
-								<a
-									href="mailto:letecnez@gmail.com"
-									className="text-light"
-									onClick={this.handleEmailClick}
-								>
-									<u>Email</u>
-								</a>
-							</li>
-							<li>
-								<a
-									href="https://www.linkedin.com/in/damjanko/"
-									className="text-light"
-									target="_blank"
-									rel="noopener noreferrer"
-									onClick={this.handleLinkedInClick}
-								>
-									<u>LinkedIn</u>
-								</a>
-							</li>
-							<li>
-								<a
-									href="https://www.facebook.com/letecnez"
-									className="text-light"
-									target="_blank"
-									rel="noopener noreferrer"
-									onClick={this.handleFacebookClick}
-								>
-									<u>Facebook</u>
-								</a>
-							</li>
-							<li>
-								<a
-									href="https://github.com/Stari89"
-									className="text-light"
-									target="_blank"
-									rel="noopener noreferrer"
-									onClick={this.handleGitHubClick}
-								>
-									<u>GitHub</u>
-								</a>
-							</li>
-						</ul>
-					</Col>
-				</Row>
-			</Container>
-		);
-	}
+  handleGitLabClick() {
+    ReactGA.event({ category: "footer", action: "gitlab-click" });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row className="py-4">
+          <Col xs="12" md="6">
+            <h3>
+              <small>About</small>
+            </h3>
+            <p>
+              This site was built with React.js. It is hosted by an Apache
+              server, running on my personal NAS device.
+            </p>
+            <p>
+              <a
+                href="https://github.com/Stari89/damjanko-homepage"
+                className="text-light"
+                onClick={this.handle}
+              >
+                <u>Pull this project from GitHub!</u>
+              </a>
+            </p>
+          </Col>
+          <Col xs="6" md="3">
+            <h3>
+              <small>Navigation</small>
+            </h3>
+            <ul className="list-unstyled">
+              <li>
+                <HashLink
+                  to="/#introduction"
+                  className="text-light"
+                  onClick={this.handleIntroductionClick}
+                >
+                  <u>Introduction</u>
+                </HashLink>
+              </li>
+              <li>
+                <HashLink
+                  to="/#experience"
+                  className="text-light"
+                  onClick={this.handleExperienceClick}
+                >
+                  <u>Experience</u>
+                </HashLink>
+              </li>
+              <li>
+                <HashLink
+                  to="/#skills"
+                  className="text-light"
+                  onClick={this.handleSkillsClick}
+                >
+                  <u>Skills</u>
+                </HashLink>
+              </li>
+            </ul>
+          </Col>
+          <Col xs="6" md="3">
+            <h3>
+              <small>Contact me</small>
+            </h3>
+            <ul className="list-unstyled">
+              <li>
+                <a
+                  href="mailto:letecnez@gmail.com"
+                  className="text-light"
+                  onClick={this.handleEmailClick}
+                >
+                  <u>Email</u>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/damjanko/"
+                  className="text-light"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={this.handleLinkedInClick}
+                >
+                  <u>LinkedIn</u>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.facebook.com/letecnez"
+                  className="text-light"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={this.handleFacebookClick}
+                >
+                  <u>Facebook</u>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/Stari89"
+                  className="text-light"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={this.handleGitHubClick}
+                >
+                  <u>GitHub</u>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://gitlab.com/Stari89"
+                  className="text-light"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={this.handleGitLabClick}
+                >
+                  <u>GitLab</u>
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,11 @@
 import React from "react";
 import {
-	Navbar,
-	Nav,
-	NavItem,
-	NavLink,
-	Collapse,
-	NavbarToggler
+  Navbar,
+  Nav,
+  NavItem,
+  NavLink,
+  Collapse,
+  NavbarToggler
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { NavHashLink } from "react-router-hash-link";
@@ -13,138 +13,152 @@ import ReactGA from "react-ga";
 import "./Navigation.css";
 
 export class Navigation extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.toggleNavbar = this.toggleNavbar.bind(this);
-		this.state = { collapsed: true };
-	}
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = { collapsed: true };
+  }
 
-	toggleNavbar() {
-		this.setState({ collapsed: !this.state.collapsed });
-	}
+  toggleNavbar() {
+    this.setState({ collapsed: !this.state.collapsed });
+  }
 
-	handleLogoClick() {
-		ReactGA.event({ category: "nav", action: "logo-click" });
-	}
+  handleLogoClick() {
+    ReactGA.event({ category: "nav", action: "logo-click" });
+  }
 
-	handleIntroductionClick() {
-		ReactGA.event({ category: "nav", action: "introduction-click" });
-	}
+  handleIntroductionClick() {
+    ReactGA.event({ category: "nav", action: "introduction-click" });
+  }
 
-	handleExperienceClick() {
-		ReactGA.event({ category: "nav", action: "experience-click" });
-	}
+  handleExperienceClick() {
+    ReactGA.event({ category: "nav", action: "experience-click" });
+  }
 
-	handleSkillsClick() {
-		ReactGA.event({ category: "nav", action: "skills-click" });
-	}
+  handleSkillsClick() {
+    ReactGA.event({ category: "nav", action: "skills-click" });
+  }
 
-	handleEmailClick() {
-		ReactGA.event({ category: "nav", action: "email-click" });
-	}
+  handleEmailClick() {
+    ReactGA.event({ category: "nav", action: "email-click" });
+  }
 
-	handleLinkedInClick() {
-		ReactGA.event({ category: "nav", action: "linkedin-click" });
-	}
+  handleLinkedInClick() {
+    ReactGA.event({ category: "nav", action: "linkedin-click" });
+  }
 
-	handleFacebookClick() {
-		ReactGA.event({ category: "nav", action: "facebook-click" });
-	}
+  handleFacebookClick() {
+    ReactGA.event({ category: "nav", action: "facebook-click" });
+  }
 
-	handleGitHubClick() {
-		ReactGA.event({ category: "nav", action: "github-click" });
-	}
+  handleGitHubClick() {
+    ReactGA.event({ category: "nav", action: "github-click" });
+  }
 
-	render() {
-		return (
-			<Navbar color="light" light expand="md" className="fixed-top">
-				<Link to="/" className="navbar-brand" onClick={this.handleLogoClick}>
-					<img
-						src="/assets/images/logo.svg"
-						className="img-fluid logo mr-3"
-						alt="logo"
-					/>
-					DamjanKo.com
-				</Link>
-				<NavbarToggler onClick={this.toggleNavbar} />
-				<Collapse isOpen={!this.state.collapsed} navbar>
-					<Nav className="ml-auto" navbar>
-						<NavItem>
-							<NavHashLink
-								className="nav-link"
-								activeClassName="active"
-								to="/#introduction"
-								exact
-								onClick={this.handleIntroductionClick}
-							>
-								Introduction
-							</NavHashLink>
-						</NavItem>
-						<NavItem>
-							<NavHashLink
-								className="nav-link"
-								activeClassName="active"
-								to="/#experience"
-								onClick={this.handleExperienceClick}
-							>
-								Experience
-							</NavHashLink>
-						</NavItem>
-						<NavItem>
-							<NavHashLink
-								className="nav-link"
-								activeClassName="active"
-								to="/#skills"
-								onClick={this.handleSkillsClick}
-							>
-								Skills
-							</NavHashLink>
-						</NavItem>
-					</Nav>
-					<Nav className="ml-auto" navbar>
-						<NavItem>
-							<NavLink
-								href="mailto:letecnez@gmail.com"
-								className="p-1"
-								onClick={this.handleEmailClick}
-							>
-								<img src="/assets/images/ico-mail.svg" alt="E-mail" />
-							</NavLink>
-						</NavItem>
-						<NavItem>
-							<NavLink
-								href="https://www.linkedin.com/in/damjanko/"
-								className="p-1"
-								target="_blank"
-								onClick={this.handleLinkedInClick}
-							>
-								<img src="/assets/images/ico-linkedin.svg" alt="LinkedIn" />
-							</NavLink>
-						</NavItem>
-						<NavItem>
-							<NavLink
-								href="https://www.facebook.com/letecnez"
-								className="p-1"
-								target="_blank"
-								onClick={this.handleFacebookClick}
-							>
-								<img src="/assets/images/ico-facebook.svg" alt="Facebook" />
-							</NavLink>
-						</NavItem>
-						<NavItem>
-							<NavLink
-								href="https://github.com/Stari89"
-								className="p-1"
-								target="_blank"
-								onClick={this.handleGitHubClick}
-							>
-								<img src="/assets/images/ico-github.svg" alt="GitHub" />
-							</NavLink>
-						</NavItem>
-					</Nav>
-				</Collapse>
-			</Navbar>
-		);
-	}
+  handleGitLabClick() {
+    ReactGA.event({ category: "nav", action: "gitlab-click" });
+  }
+
+  render() {
+    return (
+      <Navbar color="light" light expand="md" className="fixed-top">
+        <Link to="/" className="navbar-brand" onClick={this.handleLogoClick}>
+          <img
+            src="/assets/images/logo.svg"
+            className="img-fluid logo mr-3"
+            alt="logo"
+          />
+          DamjanKo.com
+        </Link>
+        <NavbarToggler onClick={this.toggleNavbar} />
+        <Collapse isOpen={!this.state.collapsed} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavHashLink
+                className="nav-link"
+                activeClassName="active"
+                to="/#introduction"
+                exact
+                onClick={this.handleIntroductionClick}
+              >
+                Introduction
+              </NavHashLink>
+            </NavItem>
+            <NavItem>
+              <NavHashLink
+                className="nav-link"
+                activeClassName="active"
+                to="/#experience"
+                onClick={this.handleExperienceClick}
+              >
+                Experience
+              </NavHashLink>
+            </NavItem>
+            <NavItem>
+              <NavHashLink
+                className="nav-link"
+                activeClassName="active"
+                to="/#skills"
+                onClick={this.handleSkillsClick}
+              >
+                Skills
+              </NavHashLink>
+            </NavItem>
+          </Nav>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink
+                href="mailto:letecnez@gmail.com"
+                className="p-1"
+                onClick={this.handleEmailClick}
+              >
+                <img src="/assets/images/ico-mail.svg" alt="E-mail" />
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                href="https://www.linkedin.com/in/damjanko/"
+                className="p-1"
+                target="_blank"
+                onClick={this.handleLinkedInClick}
+              >
+                <img src="/assets/images/ico-linkedin.svg" alt="LinkedIn" />
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                href="https://www.facebook.com/letecnez"
+                className="p-1"
+                target="_blank"
+                onClick={this.handleFacebookClick}
+              >
+                <img src="/assets/images/ico-facebook.svg" alt="Facebook" />
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                href="https://github.com/Stari89"
+                className="p-1"
+                target="_blank"
+                onClick={this.handleGitHubClick}
+              >
+                <img src="/assets/images/ico-github.svg" alt="GitHub" />
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                href="https://gitlab.com/Stari89"
+                className="p-1"
+                target="_blank"
+                onClick={this.handleGitLabClick}
+              >
+                <img src="/assets/images/logo-gitlab.svg" alt="GitLab" />
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    );
+  }
 }
